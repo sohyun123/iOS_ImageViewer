5945358b6e690b3cbdd58bc414634d41c91456fc
Sohyun <carriesohyun@gmail.com>
2020-04-21 01:38:05 +0900

    Initial Commit

---
+++ ImageViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageViewer
+//
+//  Created by 박소현 on 2020/04/21.
+//  Copyright © 2020 Sohyun Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
