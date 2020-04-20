f03d8f3b21575fbff9e44d3ed3477e23478b1bb0
Sohyun <carriesohyun@gmail.com>
2020-04-21 02:31:51 +0900

    An image viewer that shows my pictures from Europe

--- ImageViewer/ViewController.swift
+++ ImageViewer/ViewController.swift
@@ -10,11 +10,91 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var img1: UIImage?
+    var img2: UIImage?
+    var img3: UIImage?
+    var img4: UIImage?
+    let maxNum = 4
+    var currentImg = 1
+
+    
+    @IBOutlet var imgPics: UIImageView!
+    @IBOutlet var txtPlace: UILabel!
+    @IBOutlet var txtDate: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        img1 = UIImage(named: "1.JPG")
+        img2 = UIImage(named: "2.JPG")
+        img3 = UIImage(named: "3.JPG")
+        img4 = UIImage(named: "4.JPG")
+
+        imgPics.image = img1
     }
+    
 
-
+    @IBAction func btnBack(_ sender: UIButton) {
+        if(currentImg==1)
+        {
+        imgPics.image = img1
+        txtPlace.text = "Malaga, Spain"
+        txtDate.text = "2019.09"
+        }
+        else
+        {
+            currentImg -= 1
+            if(currentImg==1)
+            {
+            imgPics.image = img1
+            txtPlace.text = "Malaga, Spain"
+            txtDate.text = "2019.09"
+            }
+            else if(currentImg==2)
+            {
+                imgPics.image = img2
+                txtPlace.text = "Paris, France"
+                txtDate.text = "2019.08"
+            }
+            else if(currentImg==3)
+            {
+                imgPics.image = img3
+                txtPlace.text = "Pargue, Czecho"
+                txtDate.text = "2019.08"
+            }
+        }
+        
+    }
+    @IBAction func btnNext(_ sender: UIButton) {
+        if(currentImg==4)
+        {
+        imgPics.image = img4
+        txtPlace.text = "Paris, France"
+        txtDate.text = "2019.08"
+        }
+        else
+        {
+            currentImg += 1
+            if(currentImg==2)
+            {
+                imgPics.image = img2
+                txtPlace.text = "Paris, France"
+                txtDate.text = "2019.08"
+            }
+            else if(currentImg==3)
+            {
+                imgPics.image = img3
+                txtPlace.text = "Pargue, Czecho"
+                txtDate.text = "2019.08"
+            }
+            else if(currentImg==4)
+                   {
+                   imgPics.image = img4
+                   txtPlace.text = "Paris, France"
+                   txtDate.text = "2019.08"
+                   }
+        }
+    }
 }
 
